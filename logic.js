a0010aeeab1060c482ade231493810b8d9a79d20
carlos <desoftsis@gmail.com>
2026-02-12 23:15:23 -0400

    Version SaaS 2.4 con mostrador de venta y pendiente

--- logic.js
+++ logic.js
@@ -10,12 +10,12 @@ const CLIENT_ID = urlParams.get('id') || 'demo'; // 'demo' es tu rifa personal
 // 2. CONFIGURACIÓN DINÁMICA
 // ⚠️ URL PRODUCCIÓN
 const BASE_API = "http://localhost:3000/api";
-const BACKEND_URL = `${BASE_API}/${CLIENT_ID}/comprar`;
-const CONFIG_URL = `${BASE_API}/${CLIENT_ID}/config`;
+//const BACKEND_URL = `${BASE_API}/${CLIENT_ID}/comprar`;
+//const CONFIG_URL = `${BASE_API}/${CLIENT_ID}/config`;
 
 // ... (El resto del archivo sigue igual) ...
-//const BACKEND_URL = "http://localhost:3000/api/comprar";
-//const CONFIG_URL = "http://localhost:3000/api/config";
+const BACKEND_URL = "http://localhost:3000/api/comprar";
+const CONFIG_URL = "http://localhost:3000/api/config";
 
 // Variables Globales
 let TICKET_PRICE = 5; 
